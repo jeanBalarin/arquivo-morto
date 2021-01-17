cdaf127ed1f98a90e2ea76c2b8b9659d7536e9ba
Jean Marcos Francisco <Jfrancisco@alunos.utfpr.edu.br>
2021-01-17 16:59:23 -0300

    firt commit

---
+++ banckend/src/main/java/br/coop/integrada/arquivomorto/ArquivoMortoApplication.java
@@ -0,0 +1,13 @@
+package br.coop.integrada.arquivomorto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArquivoMortoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArquivoMortoApplication.class, args);
+	}
+
+}
---
+++ banckend/src/test/java/br/coop/integrada/arquivomorto/ArquivoMortoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.coop.integrada.arquivomorto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArquivoMortoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
